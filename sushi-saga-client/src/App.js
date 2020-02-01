064f5182d01c7e4e00bc97909b0c3131e7f88887
Alice Won <alicewon4@gmail.com>
2020-01-31 16:14:38 -0800

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,12 +5,58 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      sushi: []
+    }
+  }
+
+
+
+componentDidMount() {
+  fetch(API)
+  .then(res=>res.json())
+  .then(json => this.allSushi(json)
+  )
+}
+
+allSushi = (sushis) => {
+  this.setState({
+    sushi: sushis.map((sushi) => ({
+      ...sushi, isEaten: false}
+
+      )
+
+    )
+  })
+  
+}
+
+
+eatSushi= (clickedSushi) => {
+  console.log("handleCllck fires")
+  let newSushiArray = [...this.state.sushi]
+  newSushiArray.map(sushi => {
+    if (clickedSushi.id === sushi.id) {
+    return  clickedSushi.isEaten = true;
+    }
+
+  })
+
+  this.setState({
+    sushi: newSushiArray
+  })
+
+}
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        {console.log(this.state)}
+        <SushiContainer eatSushi={this.eatSushi} allSushis={this.state.sushi}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+const eachSushi = () => {
+  return props.allSushis.slice(0,4).map((sushi, index) => {
+    return <Sushi eatSushi={props.eatSushi} sushi={sushi} key={index}/>
+  })
+}
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
+      {eachSushi()}
         <MoreButton />
       </div>
     </Fragment>
